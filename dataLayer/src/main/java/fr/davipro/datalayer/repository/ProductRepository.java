36f6c2f34cdcad0a0b35b9414b835af73428e055
David Atlan <david@davipro.fr>
2024-02-06 17:18:05 +0100

    ProductRepository interface was created.

---
+++ dataLayer/src/main/java/fr/davipro/datalayer/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package fr.davipro.datalayer.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import fr.davipro.datalayer.model.Product;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+    
+}
