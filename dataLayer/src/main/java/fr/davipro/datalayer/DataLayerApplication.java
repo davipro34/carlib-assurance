b18b02b8ed95120a885fd4b9230a4c7f99326960
David Atlan <david@davipro.fr>
2024-02-07 15:00:17 +0100

    Added a ManyToOne relationship between comments and products in Comment.

--- dataLayer/src/main/java/fr/davipro/datalayer/DataLayerApplication.java
+++ dataLayer/src/main/java/fr/davipro/datalayer/DataLayerApplication.java
@@ -1,26 +1,13 @@
 package fr.davipro.datalayer;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import fr.davipro.datalayer.model.Category;
-import fr.davipro.datalayer.model.Product;
-import fr.davipro.datalayer.service.CategoryService;
-import fr.davipro.datalayer.service.ProductService;
 import jakarta.transaction.Transactional;
 
 @SpringBootApplication
 public class DataLayerApplication implements CommandLineRunner {
-
-	@Autowired
-	private CategoryService categoryService;
-	
-	@Autowired
-	private ProductService productService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataLayerApplication.class, args);
@@ -29,24 +16,6 @@ public class DataLayerApplication implements CommandLineRunner {
 	@Override
 	@Transactional
 	public void run(String... args) throws Exception {
-
-		Optional<Category>optCategory = categoryService.getCategoryById(1);
-		Category categoryId1 = optCategory.get();
-		
-		System.out.println(categoryId1.getName());
-
-		categoryId1.getProducts().forEach(
-			product -> System.out.println(product.getName())
-		);
-		
-		Optional<Product> optProduct = productService.getProductById(4);
-		Product productId4 = optProduct.get();
-
-		System.out.println(productId4.getName());
-
-		productId4.getCategories().forEach(
-			category -> System.out.println(category.getName())
-		);
 
 	}
 
--- dataLayer/src/main/java/fr/davipro/datalayer/model/Comment.java
+++ dataLayer/src/main/java/fr/davipro/datalayer/model/Comment.java
@@ -1,10 +1,13 @@
 package fr.davipro.datalayer.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +21,12 @@ public class Comment {
 
     @Column(name = "contenu")
     private String content;
+
+    @ManyToOne(
+        cascade = CascadeType.ALL
+                                    )
+	@JoinColumn(name="produit_id")
+	private Product product;
 
     public int getCommentaryId() {
         return commentaryId;
@@ -33,6 +42,14 @@ public class Comment {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
     }
 
 }
