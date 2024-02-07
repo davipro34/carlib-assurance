787f62a84ee971e36ec0ad3cf131ae88aa716eb8
David Atlan <david@davipro.fr>
2024-02-07 12:25:57 +0100

    Added relationship between products and comments in Product.

--- dataLayer/src/main/java/fr/davipro/datalayer/DataLayerApplication.java
+++ dataLayer/src/main/java/fr/davipro/datalayer/DataLayerApplication.java
@@ -7,11 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import fr.davipro.datalayer.model.Category;
-import fr.davipro.datalayer.model.Comment;
 import fr.davipro.datalayer.model.Product;
-import fr.davipro.datalayer.service.CategoryService;
-import fr.davipro.datalayer.service.CommentService;
 import fr.davipro.datalayer.service.ProductService;
 
 @SpringBootApplication
@@ -19,12 +15,6 @@ public class DataLayerApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProductService productService;
-
-	@Autowired
-	private CommentService commentService;
-
-	@Autowired
-	private CategoryService categoryService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataLayerApplication.class, args);
@@ -35,15 +25,13 @@ public class DataLayerApplication implements CommandLineRunner {
 
 		Optional<Product> optProduct = productService.getProductById(1);
 		Product productId1 = optProduct.get();
+
 		System.out.println(productId1.getName());
 
-		Optional<Comment> optComment = commentService.getCommentById(1);
-		Comment commentId1 = optComment.get();
-		System.out.println(commentId1.getContent());
+		productId1.getComments().forEach(
+			comment -> System.out.println(comment.getContent())
+		);
 
-		Optional<Category> optCategory = categoryService.getCategoryById(1);
-		Category categoryId1 = optCategory.get();
-		System.out.println(categoryId1.getName());
 
 	}
 
--- dataLayer/src/main/java/fr/davipro/datalayer/model/Product.java
+++ dataLayer/src/main/java/fr/davipro/datalayer/model/Product.java
@@ -1,10 +1,17 @@
 package fr.davipro.datalayer.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +31,14 @@ public class Product {
 
     @Column(name = "cout")
     private int cost;
+
+    @OneToMany(
+        cascade = CascadeType.ALL,
+        orphanRemoval = true,
+        fetch = FetchType.EAGER)
+    @JoinColumn(name = "produit_id")
+    List<Comment> comments = new ArrayList<>();
+
 
     public int getProductId() {
         return productId;
@@ -56,6 +71,13 @@ public class Product {
     public void setCost(int cost) {
         this.cost = cost;
     }
+    
+    public List<Comment> getComments() {
+        return comments;
+    }
 
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
     
 }
