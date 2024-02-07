129c5cbe9ac40723104b4a2d287ddf5c27f6bb54
David Atlan <david@davipro.fr>
2024-02-07 09:12:20 +0100

    Ajout de l'injection de dépendance ProductService dans DataLayerApplication.java

--- dataLayer/src/main/java/fr/davipro/datalayer/DataLayerApplication.java
+++ dataLayer/src/main/java/fr/davipro/datalayer/DataLayerApplication.java
@@ -1,13 +1,25 @@
 package fr.davipro.datalayer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.davipro.datalayer.service.ProductService;
+
 @SpringBootApplication
-public class DataLayerApplication {
+public class DataLayerApplication implements CommandLineRunner {
+	
+	@Autowired
+	private ProductService productService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataLayerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 
 }
